41ad75ab2b70058cb799130ea904865d5ccd65af
zongren <zongren@live.com>
2023-06-21 17:35:25 +0800

    fix timezone bug,see #24

---
+++ command.sh
@@ -0,0 +1,4 @@
+./src/adbsync.py --adb-option s 192.168.6.154:5555 push /Users/zongren/Downloads/Music/ /sdcard/Music
+./src/adbsync.py --adb-option s 192.168.6.154:5555 --dry-run push /Users/zongren/Downloads/Music/ /sdcard/Music
+./src/adbsync.py --adb-option s 192.168.6.154:5555 push /Users/zongren/Downloads/MusicTemp/ /sdcard/MusicTemp 
+./src/adbsync.py --adb-option s 192.168.6.154:5555 --dry-run push /Users/zongren/Downloads/MusicTemp/ /sdcard/MusicTemp 
